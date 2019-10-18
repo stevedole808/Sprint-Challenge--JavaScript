1bd3c6dcf47ff88fa64f7df3847fc61256665b08
stevedole808 <stevedolelambda@gmail.com>
2019-10-18 07:37:13 -1000

    finished mvp and classes stretch

--- challenges/classes.js
+++ challenges/classes.js
@@ -24,3 +24,25 @@ console.log(cub.volume()); // 100
 console.log(cub.surfaceArea()); // 130
 
 // Stretch Task: Extend the base class CuboidMaker with a sub class called CubeMaker.  Find out the formulas for volume and surface area for cubes and create those methods using the dimension properties from CuboidMaker.  Test your work by logging out your volume and surface area.
+
+class CubeMaker extends Maker{
+    constructor(attributes){
+    super(attributes);
+    }
+    cubeV() {
+        return this.length * this.height * this.width;
+      }
+      cubeSA() {
+        return 6 * this.width * this.height;
+      }
+}
+
+
+const cubes = new CubeMaker ({
+    length: '5',
+    width: '5',
+    height: '5'
+})
+
+console.log(cubes.cubeV()); //125
+console.log(cubes.cubeSA()); //150
--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -135,6 +135,13 @@ The zoos want to display both the scientific name and the animal name in front o
 
 */
 const displayNames = [];
+zooAnimals.forEach(function(currentValue) {
+  displayNames.push(
+    `Name: ${currentValue.animal_name}, scientific: ${
+      currentValue.scientific_name
+    }.`
+  );
+  });
 console.log(displayNames);
 
 /* Request 2: .map()
@@ -142,27 +149,31 @@ console.log(displayNames);
 The zoos need a list of all their animal's names (animal_name only) converted to lower case. Using map, create a new array of strings named lowCaseAnimalNames, each string following this pattern: "jackal, asiatic". Log the resut.
 
 */
-
-const lowCaseAnimalNames = [];
-console.log(lowCaseAnimalNames);
+const lowerCaseAnimalNames = zooAnimals.map(function(currentValue) {
+  return currentValue.animal_name.toLowerCase();
+});
+console.log(lowerCaseAnimalNames);
 
 /* Request 3: .filter() 
 
 The zoos are concerned about animals with a lower population count. Using filter, create a new array of objects called lowPopulationAnimals which contains only the animals with a population less than 5.
 
 */
-const lowPopulationAnimals = [];
-console.log(lowPopulationAnimals);
+const largerPopulation = zooAnimals.filter(function(currentValue) {
+  return currentValue.population < 5;
+});
+console.log(largerPopulation);
 
 /* Request 4: .reduce() 
 
 The zoos need to know their total animal population across the United States. Find the total population from all the zoos using the .reduce() method. Remember the reduce method takes two arguments: a callback (which itself takes two args), and an initial value for the count.
 
 */
-const populationTotal = 0;
+
+const populationTotal = zooAnimals.reduce(function(a, b) {
+  return a + b.population;
+}, 0);
 console.log(populationTotal);
-
-
 /*
 
 Stretch: If you haven't already, convert your array method callbacks into arrow functions.
